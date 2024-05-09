6e4f3fdf8151d12cef3afc785022150d5f0d4177
Pooria Samarbafzadeh <yusufyuzlu2008@gmail.com>
2024-05-09 06:33:05 -0400

    finished chapter 3

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,26 +1,21 @@
 @extends('layouts.app')
-
 @section('jumbotron')
-        <div class="jumbotron">
-                <div class="container">
-                        <h1>Events</h1>
-                        <h2>Find an event that interests you!</h2>
-                </div>
+    <div class="jumbotron">
+        <div class="container">
+            <h1>Events</h1>
+            <h2>Find an event that interests you!</h2>
         </div>
+    </div>
 @endsection
-
 @section('content')
+<h1>Events</h1>   
+<ul>
+    @forelse ($events as $event)
+    <li>{{ $event->name }}</li>
+    @empty
+    <li>No events found!</li>
+    @endforelse
 
-        <div class="row">
-                <div class="col">
-
-                        <ul>
-                                @foreach ($events as $event)
-                                        <li>{{ $event }}</li>
-                                @endforeach
-                        </ul>
-
-                </div>
-        </div>
-
+</ul>
+{!! $events->links('vendor.pagination.bootstrap-4') !!} 
 @endsection
